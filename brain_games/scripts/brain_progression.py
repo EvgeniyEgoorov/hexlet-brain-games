c8330a24e93037ebb7945701c95182ce09701f60
Eugene Egorov <egorovzhenya@gmail.com>
2023-06-10 23:02:10 +0400

    Add brain-progression game

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import random
+import prompt
+
+
+def main():
+    print("Welcome to the Brain Games!")
+
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}")
+
+    print("What number is missing in the progression?")
+    correct_answers_counter = 0
+
+    while correct_answers_counter != 3:
+        start = random.randint(1, 30)
+        step = random.randint(1, 5)
+        proggression = [str(start + step*i) for i in range(10)]
+        hidden_el_index = random.randint(0, 9)
+        hidden_el = proggression[hidden_el_index]
+        proggression[hidden_el_index] = ".."
+        progression= ' '.join(proggression)
+
+        print(f"Question: {progression}")
+
+        answer = prompt.string("Your answer: ")
+
+        if answer == hidden_el: 
+            print("Correct!")
+            correct_answers_counter += 1
+        else:
+            print(
+                f"{answer} is wrong answer ;(. Correct answer was {hidden_el}.\
+                \nLet's try again, {name}!"
+            )
+            correct_answers_counter = 0
+
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    main()
